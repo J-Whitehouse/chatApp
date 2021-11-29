6ff2716798e0e3712fc31a463efe8220e6b3ac30
Jarret Whitehouse <jarrwhit.iu.edu>
2021-11-29 17:10:20 -0500

    Initial Commit

---
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Whitehouse, Jarret M on 11/29/21.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
